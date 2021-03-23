1a39bde887e5227f6dab99921f0bf1957080acc8
Ahmed <ahmedali.96@hotmail.com>
2021-03-22 22:32:05 -0400

    added js script to title

---
+++ script.js
@@ -0,0 +1,11 @@
+const toggleNavButton = () => {
+  let titleButton = document.querySelectorAll("#navLink");
+
+  titleButton.forEach(function (button) {
+    if (button.style.display === "revert") {
+      button.style.display = "none";
+    } else {
+      button.style.display = "revert";
+    }
+  });
+};
